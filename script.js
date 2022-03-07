b9854c3af669fc3b8e1accf58614f1467df498e0
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-07 02:57:01 -0500

    commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+// console.log(generateBtn)
+
+
+const myArrayUpper = Array.from(Array(26)).map((e,i) => i + 65);
+const alphabetUpper = myArrayUpper.map((x) => String.fromCharCode(x));
+
+const myArrayLower = Array.from(Array(26)).map((e,i) => i + 97);
+const alphabetLower = myArrayLower.map((x) => String.fromCharCode(x));
+
+const numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const specChar = ["!", "@", "#", "$", "%", "^", "&","*", "(", ")"];
+
+
+function generatePassword() {
+var results = "";
+var numberOfCharacters = window.prompt("How long would you like your password to be?");
+var charQty = parseInt(numberOfCharacters);
+
+  if(charQty > 7 && charQty < 129) {
+    var upperCase = window.confirm("Would you like to include upper case letters?");
+    var lowerCase = window.confirm("How about lower case?");
+    var numeric  = window.confirm("Include numbers?");
+    var specCar = window.confirm("Special Characters?");
+  
+    if (upperCase == true) {
+      for (i = 0; i < charQty; i++) {
+        results += alphabetUpper[Math.floor(Math.random()*26)];
+      }
+    }
+  } 
+  else {
+    window.alert("That is an invalid entry. Select a length between 8 and 128");
+    return generatePassword();
+  }
+
+
+
+  return results;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+generateBtn.addEventListener("click", writePassword);
