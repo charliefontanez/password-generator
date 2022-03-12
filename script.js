e379689331716d38d54f27089a6e54616b58f0c7
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-11 23:58:49 -0500

    added generate function

--- script.js
+++ script.js
@@ -1,42 +1,84 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// console.log(generateBtn)
 
 
-const myArrayUpper = Array.from(Array(26)).map((e,i) => i + 65);
-const alphabetUpper = myArrayUpper.map((x) => String.fromCharCode(x));
+const alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const alphabetLower = "abcdefghijklmnopqrstuvwxyz";
 
-const myArrayLower = Array.from(Array(26)).map((e,i) => i + 97);
-const alphabetLower = myArrayLower.map((x) => String.fromCharCode(x));
-
-const numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const specChar = ["!", "@", "#", "$", "%", "^", "&","*", "(", ")"];
-
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()";
 
 function generatePassword() {
-var results = "";
-var numberOfCharacters = window.prompt("How long would you like your password to be?");
-var charQty = parseInt(numberOfCharacters);
+  var results = "";
+  var passWordSize = window.prompt("How long would you like your password to be?");
+  var charQty = parseInt(passWordSize);
 
-  if(charQty > 7 && charQty < 129) {
+  if (charQty > 7 && charQty < 129) {
     var upperCase = window.confirm("Would you like to include upper case letters?");
     var lowerCase = window.confirm("How about lower case?");
     var numeric  = window.confirm("Include numbers?");
-    var specCar = window.confirm("Special Characters?");
+    var specialCharacters = window.confirm("Special Characters?");
   
-    if (upperCase == true) {
-      for (i = 0; i < charQty; i++) {
-        results += alphabetUpper[Math.floor(Math.random()*26)];
+    var i = 0;
+    while (i < charQty) {
+
+    if (upperCase) {
+      results += alphabetUpper[Math.floor(Math.random() * alphabetUpper.length)];
+      i++;
+      if (i == charQty) {
+        break;
       }
     }
+    if (lowerCase) {
+      results += alphabetLower[Math.floor(Math.random() * alphabetLower.length)];
+      i++;
+      if (i == charQty) {
+        break;
+      }
+    }
+    if (numeric) {
+      results += numbers[Math.floor(Math.random() * numbers.length)];
+      i++;
+      if (i == charQty) {
+        break;
+      }
+    }
+    if (specialCharacters) {
+      results += symbols[Math.floor(Math.random() * symbols.length)];
+      i++;
+      if (i == charQty) {
+        break;
+      }
+    }
+
+
+  }
+    // var i = 0;
+    // while (i < charQty) {
+      
+    //   if (upperCase == true) {
+    //     results += alphabetUpper[Math.floor(Math.random() * alphabetUpper.length)];
+    //     i++;
+    //   }
+    //   if (lowerCase == true) {
+    //     results += alphabetLower[Math.floor(Math.random() * alphabetLower.length)];
+    //     i++;
+    //   }
+    //   if (numeric == true) {
+    //     results += numbers[Math.floor(Math.random() * numbers.length)];
+    //     i++;
+    //   }
+    //   if (specialCharacters == true) {
+    //     results += symbols[Math.floor(Math.random() * symbols.length)];
+    //     i++;
+    //   }
+    // }
+    console.log(results);
   } 
   else {
     window.alert("That is an invalid entry. Select a length between 8 and 128");
     return generatePassword();
   }
-
-
-
   return results;
 }
 
